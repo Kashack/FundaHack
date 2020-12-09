6785831e46212cc4781b5abe7929fc7704e5e0ad
Elias <kasumu.elias@gmail.com>
2020-12-09 21:07:54 +0100

    initial commit

---
+++ app/src/main/java/com/example/fundahack/IntroPage.java
@@ -0,0 +1,14 @@
+package com.example.fundahack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_page);
+    }
+}
